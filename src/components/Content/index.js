fc8189388c0ca096e7389ddc770d52d94ec8260d
Ridho Gusti <ridho@kotakmakan.id>
2019-12-06 10:55:27 +0700

    add banner

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import HomeContentSection1 from "./HomeContentSection1";
 import BannerBackgorund from "./BannerBackgorund";
+import { StyledBillboardBanner } from "./StyledContent";
 
 const Content = () => {
   return (
     <div>
       <HomeContentSection1></HomeContentSection1>
       <BannerBackgorund></BannerBackgorund>
+      <StyledBillboardBanner>
+        <h4>BILLBOARD banner</h4>
+        <h5>970x250 pixels</h5>
+      </StyledBillboardBanner>
     </div>
   );
 };
